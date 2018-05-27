bda6a6ae687987868e101ee4051a40a60086b1c3
allenfortner <allenfortner99@gmail.com>
2018-05-27 19:38:09 -0400

    Add card matching functions

--- js/app.js
+++ js/app.js
@@ -3,22 +3,24 @@ let cards = ["fa fa-diamond", "fa fa-diamond", "fa fa-paper-plane-o", "fa fa-pap
  
 let deck = $(".deck");
 let card = $(".card");
- 
-let moves = 0;
-let seconds = 0;
-const twoStars = 20;
-const oneStar = 28;
 
+let openCards = []; //Contains currently open cards, cleared after two are selected [see flipCard() and checkCards()]
+let openCardClasses = []; //Contains card picture classes, compared to check if cards match, cleared after two are selected [see flipCard() and checkCards()]
+let moves = 0; //Number of moves that have been made
+let seconds = 0; //Number of seconds since game began
+let canSelect = true; //Used to prevent users from selecting cards during timeout after wrong match
+const twoStars = 20; //Limit for 3-star score
+const oneStar = 28; //Limit for 2-star score
 
 //Create card element and add HTML inside the deck
- function createCard(cardClass) {
+function createCard(cardClass) {
 	deck.append(`<li class="card"><i class="${cardClass}"></i></li>`);
- }
+}
  
  //Shuffles card list, creates each card, and adds their HTML to the page
- function generateDeck() {
+function generateDeck() {
     shuffle(cards).forEach(createCard);
- }
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -38,7 +40,6 @@ function shuffle(array) {
 //Generate new deck when the DOM is loaded
 $(document).ready(function() {
     generateDeck();
-	//console.log("Deck has been generated!");
 	$(".card").click(flipCard);
 });
 
@@ -54,5 +55,38 @@ $(document).ready(function() {
  */
 
 function flipCard() {
-    $(this).addClass("open show");
+	if (canSelect == true){
+		$(this).addClass("open show"); //"Flips" over the card
+		openCards.push($(this)); //Adds the clicked card into openCards list
+		openCardClasses.push($(this).children().attr("class")); //Adds the clicked card's image into openCardClasses list
+		checkCards();
+	}
+}
+
+function checkCards() {
+	if (openCardClasses.length == 2) {
+		canSelect = false;
+		if (openCardClasses[0] == openCardClasses[1]) {
+			console.log("You got a match!")
+			openCards[0].toggleClass("open show match");
+			openCards[1].toggleClass("open show match");
+			
+			openCards = [];
+			openCardClasses = [];
+			canSelect = true;
+		} else {
+			console.log("Not a match...")
+			
+			setTimeout(function() {
+				openCards[0].toggleClass("open show");
+				openCards[1].toggleClass("open show");
+				
+				openCards = [];
+				openCardClasses = [];
+				canSelect = true;
+			}, 1000);
+		}
+	} else {
+        $(this).addClass("open show");
+	}
 }
